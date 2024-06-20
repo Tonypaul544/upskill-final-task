fec5dc21d1d718bcca9254503d28c97dbc1e1933
Paul Nwodo <145398513+Tonypaul544@users.noreply.github.com>
2024-06-20 17:41:58 +0100

    Create nav.js

---
+++ nav.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+
+const navbar = document.getElementById('navbar');
+const hideBtn = document.getElementById('hide-navbar-btn');
+
+hideBtn.addEventListener('click', () => {
+  navbar.style.display = 'block';
+});
+
+hideBtn.addEventListener('click', () => {
+  navbar.style.display = 'none';
+});
+
+
